419f4bd13f696d0c9656e1edd431ec13adfe4e7c
calvincho95 <calvincho95@naver.com>
2024-05-29 11:53:27 +0900

    second commit

--- src/components/TodoCard.tsx
+++ src/components/TodoCard.tsx
@@ -1,10 +1,10 @@
-import { Box, Button, Text } from "@chakra-ui/react";
+import { Box, Button, Flex, Text } from "@chakra-ui/react";
 import { FC } from "react";
 import { FiEdit3, FiTrash2 } from "react-icons/fi";
 
 const TodoCard: FC = () => {
   return (
-    <Box bgColor="gray.200" px={4} py={4} rounded="xl" gap={4}>
+    <Flex bgColor="gray.200" px={4} py={4} rounded="xl" gap={4}>
       <Text fontSize={20} bgColor="blue.100" p={4} w={48} isTruncated={true}>
         🏃‍♀️ 부쳐핸섭
       </Text>
@@ -14,7 +14,7 @@ const TodoCard: FC = () => {
       <Button colorScheme="red">
         <FiTrash2 />
       </Button>
-    </Box>
+    </Flex>
   );
 };
 
